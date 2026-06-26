079ceae8c1de99af9e0650ae5013bc3a7f56ba24
anbuselvams0206-source <anbuselvams0206@gmail.com>
2026-06-26 18:14:14 +0530

    Add MongoDB database connection
    Created database configuration file for connecting backend with MongoDB.

---
+++ frontend/frontend/frontend/backend/backend/backend/config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+
+    try {
+
+        await mongoose.connect(process.env.MONGO_URL);
+
+        console.log("Database Connected");
+
+    } catch(error){
+
+        console.log(error);
+
+    }
+
+};
+
+
+module.exports = connectDB;
